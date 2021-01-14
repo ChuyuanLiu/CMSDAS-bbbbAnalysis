cbca149b767279bc9eeed7934e2ad0fe96048bdd
ChuyuanLiu <chuyuanl@andrew.cmu.edu>
2021-01-14 10:20:49 -0600

    add applying trained BDT model

--- mldiscr/apply_bdt.py
+++ mldiscr/apply_bdt.py
@@ -8,13 +8,30 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib
 from matplotlib import pyplot as plt
 import pickle
+import root_pandas
 
-infile   = open('bdt_training.pkl', 'rb')
-in_data   = pickle.load(infile)
+infile = open('bdt_training.pkl', 'rb')
+in_data = pickle.load(infile)
 infile.close()
-
-print '... opened model, dict contains: ', in_data
+    
+print '... opened model, variables are', in_data['vars']
 model         = in_data['model']
 vars_training = in_data['vars']
 
-#### do things here
+def apply_bdt(input_path, output_path = ""):
+    input_arrs = uproot.open(input_path)['bbbbTree']
+    input_data = input_arrs.pandas.df(vars_training)
+    
+    print '... applying bdt score'
+    pred = model.predict_proba(input_data)[:,-1]
+    
+    filename = input_path.split('/')[-1]
+    filename = filename.replace('.root','_mva.root')
+    
+    print '... saving bdt score'
+    output_arrs = input_arrs.pandas.df()
+    output_arrs["bdt_score"]=pred
+    output_arrs.to_root(output_path + filename, key="bbbbTree")
+
+input_path='../analysis/objects_gg_HH_bbbb_SM.root'
+apply_bdt(input_path)
